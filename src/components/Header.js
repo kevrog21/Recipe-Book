ef35ffee15c43d556ccc3218fa29d726beac05be
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2023-05-11 15:37:54 -0700

    added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@
 export default function Header() {
     return (
         <header>
-            <h1 class='app_title'>Recipe<br />Book</h1>
+            <h1 class='app_title'>Recipe<br />Box</h1>
+            <div class='hamburger_wrapper'>
+                
+            </div>
 
 
         </header>
