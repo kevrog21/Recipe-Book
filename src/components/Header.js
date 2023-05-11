75f8d9de9de0f6edf9e8c8c47d91a5321690bf40
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2023-05-11 01:57:55 -0700

    Basic setup, font install

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <h1 class='app_title'>Recipe<br />Book</h1>
+
+
+        </header>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export default [{
+        id: '01',
+        recipeName: 'Tucci Meal',
+        recipeImage: '',
+        cookTimeMins: 90,
+        isFavorited: true,
+        isRequested: false,
+        ingredients: [{
+            measurement: '2 tablespoons',
+            ingredientName: 'butter',
+            ingredientNote: '',
+            isOptional: false
+        }],
+        instructions: 'paste instructions here',
+        notes: [
+            'Note 1 is here',
+            'Note 2 then goes here'
+        ],
+        stats: [{
+            overallScore: 8,
+            nutrition: 9,
+            cookTime: 7,
+            cleanup: 5,
+            cost: 10,
+            totalTimesCooked: 0,
+            lastCooked: '5/21/23'
+        }],
+        tags: ['vegetarian', 'cozy meal', 'Dairy Free', "Healthy"]
+    },
+
+    {
+        id: '02',
+        recipeName: 'Steak Kabobs',
+        recipeImage: '',
+        cookTimeMins: 60,
+        isFavorited: true,
+        isRequested: false,
+        ingredients: [{
+            measurement: '2 tablespoons', 
+            ingredientName: 'butter',
+            ingredientNote: '',
+            isOptional: false
+        }],
+        instructions: 'paste instructions here',
+        notes: [
+            'Note 1 is here',
+            'Note 2 then goes here'
+        ],
+        stats: [{
+            overallScore: 8,
+            nutrition: 9,
+            cookTime: 7,
+            cleanup: 5,
+            cost: 10,
+            totalTimesCooked: 0,
+            lastCooked: '5/21/23'
+        }],
+        tags: ['vegetarian', 'cozy meal', 'Dairy Free', "Healthy"]
+    }
+]
