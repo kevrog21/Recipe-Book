35e3bd7ee0f2f03d6d0d5a05ebd7d57d839474f2
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2023-06-13 22:15:08 -0700

    signed image upload success!

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -2,8 +2,25 @@ import { Router } from "express"
 import Recipe from '../models/recipes.model.js'
 import cloudinary from 'cloudinary'
 import cors from 'cors'
+import express from "express"
+import dotenv from 'dotenv'
+// import bodyParser from "body-parser"
+
+dotenv.config()
 
 const router = Router()
+
+router.use(express.static("public"))
+router.use(express.json())
+// router.use(bodyParser.raw({ type: 'multipart/form-data' }))
+
+
+// const express = require("express")
+// const app = express()
+// app.use(express.static("public"))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+
 
 router.use(cors())
 
@@ -32,26 +49,90 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/get-signature').get((req, res) => {
-    console.log('server code started')
-        const timestamp = Math.round(new Date().getTime() / 1000)
-        const signature = cloudinary.utils.api_sign_request(
-            {
-                timestamp: timestamp
-            },
-            cloudinaryConfig.api_secret
-        )
-        res.json({ timestamp, signature })
-        console.log('server code finished')
-})
+// const signatureResponse = await axios.get("http://localhost:5000/get-signature")
+
+router.get("/get-signature", (req, res) => {
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const signature = cloudinary.utils.api_sign_request(
+      {
+        timestamp: timestamp
+      },
+      cloudinaryConfig.api_secret
+    )
+    res.json({ timestamp, signature })
+  })
+
+
+router.get("/get-signature", (req, res) => {
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const signature = cloudinary.utils.api_sign_request(
+      {
+        timestamp: timestamp
+      },
+      cloudinaryConfig.api_secret
+    )
+    res.json({ timestamp, signature })
+  })
+
+
+
+router.post("/do-something-with-photo", async (req, res) => {
+    // based on the public_id and the version that the (potentially malicious) user is submitting...
+    // we can combine those values along with our SECRET key to see what we would expect the signature to be if it was innocent / valid / actually coming from Cloudinary
+    const expectedSignature = cloudinary.utils.api_sign_request({ public_id: req.body.public_id, version: req.body.version }, cloudinaryConfig.api_secret)
+  
+    // We can trust the visitor's data if their signature is what we'd expect it to be...
+    // Because without the SECRET key there's no way for someone to know what the signature should be...
+    if (expectedSignature === req.body.signature) {
+      // Do whatever you need to do with the public_id for the photo
+      // Store it in a database or pass it to another service etc...
+      await fse.ensureFile("./data.txt")
+      const existingData = await fse.readFile("./data.txt", "utf8")
+      await fse.outputFile("./data.txt", existingData + req.body.public_id + "\n")
+    }
+  })
+
+// const signatureResponse = await axios.get("http://localhost:5000/get-signature")
+
+// app.get("/get-signature", (req, res) => {
+//     const timestamp = Math.round(new Date().getTime() / 1000)
+//     const signature = cloudinary.utils.api_sign_request(
+//       {
+//         timestamp: timestamp
+//       },
+//       cloudinaryConfig.api_secret
+//     )
+//     res.json({ timestamp, signature })
+//   })
+
 
 router.route('/test-endpoint').get((req, res) => {
     res.json({message: 'This is a test endpoint'})
 })
 
-router.route('/upload-image').get((req, res) => {
-    cloudinary.v2.uploader.upload(file, options).then()
-       
+router.route('/upload-image').post((req, res) => {
+    console.log(req.body)
+
+    // const body = {
+    //     file: req.body.file,
+    //     timestamp: req.body.timestamp,
+    //     api_key: req.body.api_key,
+    //     signature: req.body.signature
+    // }
+    const formData = parseFormData(req.body.toString())
+
+    console.log("formData", formData)
+
+    const options = {
+        headers: { "Content-Type": "multipart/form-data" },
+        api_key: formData.api_key
+    }
+
+    console.log(options)
+
+    cloudinary.uploader.upload(req.body, options)
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
 })
 
 router.route('/add').post((req, res) => {
--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -8,13 +8,13 @@ const RecipesList = props => {
 
     const [recipeData, setRecipeData] = useState([])
 
-    useEffect(() => {
-        const testResponse = fetch('http://localhost:5000/recipes/test-endpoint')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
-    }, [])
+    // useEffect(() => {
+    //     const testResponse = fetch('http://localhost:5000/recipes/test-endpoint')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    // }, [])
 
     useEffect(() => {
         retrieveRecipes()
@@ -46,7 +46,7 @@ const RecipesList = props => {
 
     const api_key = "124659146613462"
     const cloud_name = "dot31xj56"
-
+    // const CLOUDINARY_PRESET = "khjbvoiy"
 
     // const [image, setImage] = useState(null)
 
@@ -54,43 +54,48 @@ const RecipesList = props => {
     const [finalImageObject, setFinalImageObject] = useState({})
     const [isInitialRender, setIsInitialRender] = useState(true)
 
-    useEffect(() => {
-        if (!isInitialRender) {
-            console.log(finalImageObject)
+//     useEffect(() => {
+//         const postData = async () => {
+//             if (!isInitialRender) {
+//                 console.log(finalImageObject)
 
-            const formData = new FormData()
-            formData.append("file", finalImageObject.file)
-            formData.append("api_key", api_key)
-            formData.append("signature", finalImageObject.signature)
-            formData.append("timestamp", finalImageObject.timestamp)
-            formData.append("upload_preset", 'khjbvoiy' )
+//                 const formData = new FormData()
+//                 formData.append("file", finalImageObject.file)
+//                 formData.append("api_key", api_key)
+//                 formData.append("signature", finalImageObject.signature)
+//                 formData.append("timestamp", finalImageObject.timestamp)
+//                 // formData.append("upload_preset", 'khjbvoiy' )
 
-            console.log("formData entries")
-            for (const entry of formData.entries()) {
-                console.log(entry)
-            }
+//                 console.log("formData entries")
+//                 for (const entry of formData.entries()) {
+//                     console.log(entry)
+//                 }
 
-            const requestOptions = {
-                method: "POST",
-                body: formData,
-                headers: { "Content-Type": "multipart/form-data" }
-                // onUploadProgress: function (e) {
-                //     console.log(e.loaded / e.total)
-                // }
-            }
-            console.log("body:", requestOptions.body)
-            const cloudinaryResponse = fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {...requestOptions})
-            cloudinaryResponse.then(response => response.json())
-            cloudinaryResponse.then(data => {
-                console.log(data)
-            })
-            .catch(error => {
-                console.error(error)
-            })
-        } else {
-            setIsInitialRender(false)
-        }
-    }, [finalImageObject])
+//                 const requestOptions = {
+//                     method: "POST",
+//                     body: formData,
+//                     headers: { "Content-Type": "multipart/form-data" },
+//                     onUploadProgress: function (e) {
+//                         console.log(e.loaded / e.total)
+//                     }
+//                 }
+
+// //https://api.cloudinary.com/v1_1/${cloud_name}/image/upload
+
+//                 console.log("body:", requestOptions.body)
+//                 try {
+//                     const cloudinaryResponse = await axios.post(`http://localhost:5000/recipes/upload-image`, formData, requestOptions)
+//                     const data = await cloudinaryResponse.data.json()
+//                     console.log(data)
+//                 } catch(error) {
+//                     console.error(error)
+//                 }
+//             } else {
+//                 setIsInitialRender(false)
+//             }
+//         }
+//         postData()
+//     }, [finalImageObject])
 
     const handleImageChange = (e) => {
         const selectedImage = e.target.files[0]
@@ -110,67 +115,98 @@ const RecipesList = props => {
         }))
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const options = {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
+        // const options = {
+        //     method: "POST",
+        //     body: JSON.stringify(formData),
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     }
+        // }
 
         // if image, get signature and add it to form data to be sent to the server.
 
         // check if a file is included in form
 
-        let signatureResponse = {}
+        // let signatureResponse = {}
 
         if (imageObject.file) {
-            // make a get request to our own server at /get-signature
-            console.log(typeof imageObject.file)
-            signatureResponse = fetch("http://localhost:5000/recipes/get-signature")
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
+            try {
+                const signatureResponse = await axios.get("http://localhost:5000/recipes/get-signature")
+                const data = new FormData()
+                data.append("file", document.querySelector("#image").files[0])
+                data.append("api_key", api_key)
+                data.append("signature", signatureResponse.data.signature)
+                data.append("timestamp", signatureResponse.data.timestamp)
 
-                    console.log(imageObject)
-                    setFinalImageObject((prevData) => ({
-                        ...prevData,
-                        file: imageObject.file,
-                        api_key: api_key,
-                        signature: data.signature,
-                        timestamp: data.timestamp
-                    }))
-                   
-                    // .then(() => {
-                    //     console.log(imageObject)
-                        
-                    //     const cloudinaryResponse = fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
-                    //         method: "POST",
-                    //         body: JSON.stringify(imageObject),
-                    //         headers: { "Content-Type": "multipart/form-data" },
-                    //         onUploadProgress: function (e) {
-                    //             console.log(e.loaded / e.total)
-                    //         }
-                    //     })
-                    //     cloudinaryResponse.then(response => response.json())
-                    //     cloudinaryResponse.then(data => {
-                    //         console.log(data)
-                    //     })
-                    //     .catch(error => {
-                    //         console.error(error)
-                    //     })
-                    //     console.log(cloudinaryResponse.data)
-                    // })
+                for (const entry of data.entries()) {
+                    console.log(entry)
+                    }
 
+                // console.log(data)
+                // console.log(imageObject)
+
+                const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`, data, {
+                    headers: { "Content-Type": "multipart/form-data" },
+                    onUploadProgress: function (e) {
+                    console.log(e.loaded / e.total)
+                    }
                 })
-                .catch(error => {
-                    console.error(error)
-                })
-                console.log("image code ran")
+                console.log(cloudinaryResponse.data)
+
+            //     setFinalImageObject((prevData) => ({
+            //         ...prevData,
+            //         file: imageObject.file,
+            //         api_key: api_key,
+            //         signature: data.signature,
+            //         timestamp: data.timestamp
+            //     }))
+            // } catch (error) {
+            //     console.log(error)
+            // }
+            // console.log("image code ran")
+
+            const photoData = {
+                public_id: cloudinaryResponse.data.public_id,
+                version: cloudinaryResponse.data.version,
+                signature: cloudinaryResponse.data.signature
+              }
+
+            } catch (error) {
+                console.log(error)
+            }
+            console.log("image code ran")
         }
+
+
+
+            
+
+
+
+            // // make a get request to our own server at /get-signature
+            // console.log(typeof imageObject.file)
+            // signatureResponse = fetch("http://localhost:5000/recipes/get-signature")
+            //     .then(response => response.json())
+            //     .then(data => {
+            //         console.log(data)
+
+            //         console.log(imageObject)
+            //         setFinalImageObject((prevData) => ({
+            //             ...prevData,
+            //             file: imageObject.file,
+            //             api_key: api_key,
+            //             signature: data.signature,
+            //             timestamp: data.timestamp
+            //         }))
+            //     })
+            //     .catch(error => {
+            //         console.error(error)
+            //     })
+            //     console.log("image code ran")
+        
 
 
         // if (imageObject.file) {
@@ -275,195 +311,3 @@ const RecipesList = props => {
 }
 
 export default RecipesList
-
-// import { Link } from 'react-router-dom'
-// import { useState, useEffect } from 'react' 
-// import RecipeDataService from '../services/recipeList'
-// import axios from 'axios'
-// import { CloudinaryContext, Image } from "cloudinary-react"
-
-// const RecipesList = props => {
-
-//     const [recipeData, setRecipeData] = useState([])
-
-//     useEffect(() => {
-//         retrieveRecipes()
-//     }, [])
-
-//     const retrieveRecipes = () => {
-//         RecipeDataService.getAll()
-//             .then(response => {
-//                 console.log(response.data)
-//                 setRecipeData(response.data)
-//             })
-//             .catch(e => {
-//                 console.log(e)
-//             })
-//     }
-
-//     const list = recipeData ? recipeData.map(recipe => <h4 key={recipe._id}>{recipe.recipeName}</h4>) : ""
-
-//     const [formData, setFormData] = useState({
-//         recipeName: '',
-//         instructions: '',
-//         cooktime: '',
-//         password: '',
-//         honeyp: ''
-//     })
-
-
-
-
-//     const api_key = "124659146613462"
-//     const cloud_name = "dot31xj56"
-
-//     // const [image, setImage] = useState(null)
-
-//     const [imageObject, setImageObject] = useState({})
-
-//     const handleImageChange = (e) => {
-//         const selectedImage = e.target.files[0]
-//         setImageObject({
-//             file: selectedImage
-//         })
-//         console.log(selectedImage)
-//     }
-
-//     // const handleImageSubmit = (e) => {
-//     //     e.preventDefault()
-//     // }
-
-
-
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target
-
-//         const isNumericField = ["cooktime"].includes(name) //returns true if the target name is included in the array
-//         setFormData((prevData) => ({
-//             ...prevData,
-//             [name]: isNumericField ? parseInt(value) : value
-//         }))
-//     }
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-
-//         const options = {
-//             method: "POST",
-//             body: JSON.stringify(formData),
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         }
-
-//         // if image, get signature and add it to form data to be sent to the server.
-
-//         // check if a file is included in form
-
-//         let signatureResponse = {}
-
-//         if (imageObject.file) {
-//             // make a get request to our own server at /get-signature
-//             console.log(imageObject.file)
-//             signatureResponse = fetch("http://localhost:5000/recipes/get-signature")
-//                 .then(response => response.json())
-//                 .then(data => {
-//                     console.log(data)
-//                     new Promise((resolve) => {
-//                         setImageObject((prevData) => ({
-//                             ...prevData,
-//                             file: imageObject.file,
-//                             api_key: api_key,
-//                             signature: data.signature,
-//                             timestamp: data.timestamp
-//                         }))
-//                         resolve()
-//                         return imageObject
-//                     })
-//                     .then(() => {
-//                         console.log(imageObject)
-                        
-//                         const cloudinaryResponse = fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
-//                             method: "POST",
-//                             body: JSON.stringify(imageObject),
-//                             headers: { "Content-Type": "multipart/form-data" },
-//                             onUploadProgress: function (e) {
-//                                 console.log(e.loaded / e.total)
-//                             }
-//                         })
-//                         cloudinaryResponse.then(response => response.json())
-//                         cloudinaryResponse.then(data => {
-//                             console.log(data)
-//                         })
-//                         .catch(error => {
-//                             console.error(error)
-//                         })
-//                         console.log(cloudinaryResponse.data)
-//                     })
-
-//                 })
-//                 .catch(error => {
-//                     console.error(error)
-//                 })
-//                 console.log("image code ran")
-//         }
-        
-//         console.log(signatureResponse)
-
-
-//         // fetch("http://localhost:5000/recipes/add", options)
-//         //     .then(res => {
-//         //         res.json()
-//         //         if (res.ok) {
-//         //             setFormData({
-//         //                 recipeName: '',
-//         //                 instructions: '',
-//         //                 cooktime: '',
-//         //                 password: '',
-//         //                 honeyp: ''
-//         //             })
-//         //             setImage(null)
-//         //         }
-//         //     })
-//         //     .then(data => console.log(data))
-//     }
-
-//     return (
-//         <div>
-//             <h2>Add Recipes</h2>
-//             {list}
-
-//             <CloudinaryContext cloudName="dot31xj56">
-//                 <div>
-//                     <Image publicId="sample" width="50" />
-//                 </div>
-//                 <Image publicId="sample" width="0.5" />
-//             </CloudinaryContext>
-
-//             <form className="add-recipe-form" onSubmit={handleSubmit}>
-//                 <label htmlFor="recipe-name">Recipe Name:</label>
-//                 <input type="text" id="recipe-name" name="recipeName" value={formData.recipeName} onChange={handleInputChange}></input>
-//                 <label htmlFor="instructions">Instructions:</label>
-//                 <textarea type="text" id="instructions" name="instructions" value={formData.instructions} onChange={handleInputChange}></textarea>
-//                 <label htmlFor="cooktime">Cooktime:</label>
-//                 <input type="number" id="cooktime" name="cooktime" value={formData.cooktime} onChange={handleInputChange}></input>
-//                 <label htmlFor="password">Secret Password:</label>
-//                 <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChange}></input>
-//                 <input type="text" id="honeyp" name="honeyp" value={formData.honeyp} onChange={handleInputChange}></input>
-//                 <label htmlFor="image">Upload Image:</label>
-//                 <input type="file" id="image" name="image" onChange={handleImageChange}/>
-//                 <button type="submit" className="submit-recipe-btn" id="submit" >Submit Recipe</button>
-//             </form>
-
-//             {/* <form className="temp-img-form" onSubmit={handleImageSubmit}>
-//                 <label htmlFor="image">Upload Image:</label>
-//                 <input type="file" id="image" name="image" onChange={handleImageChange}/>
-
-//                 <button type="submit" className="submit-image-btn" id="submitImage" >Submit Image</button>
-//             </form> */}
-
-//         </div>
-//     )
-// }
-
-// export default RecipesList
