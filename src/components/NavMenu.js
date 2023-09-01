aefc7e2dd0306c02bd5105003f9f3767f4799f41
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2023-09-01 00:49:35 -0700

    added search bar styling to nav menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,6 +4,7 @@ export default function NavMenu(props) {
     return (
         <div className='nav-menu-container'>
             <div id='nav-menu' className=''>
+                <div id='nav-search-bar'>search...</div>
                 <Link to='/' className='nav-menu-item'><div onClick={props.handleNavItemClick}>Home</div></Link>
                 <Link to='/add-recipe' className='nav-menu-item'><div onClick={props.handleNavItemClick}>Add Recipe</div></Link>
                 <Link to='/' className='nav-menu-item'><div onClick={props.handleNavItemClick}>Grocery List</div></Link>
